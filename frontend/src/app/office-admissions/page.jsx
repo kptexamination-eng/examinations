12daa24a288b8bf35e0a0059bcb55572003942eb
kptudyogamela <kptudyogamela@gmail.com>
2025-11-17 16:20:35 +0530

    first delete

---
+++ frontend/src/app/office-admissions/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import Link from "next/link";
+import Navbar from "../components/Navbar";
+
+export default function AdminHome() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-blue-800 mb-4">
+        {/* Welcome, Admin 👋 */}
+      </h1>
+    </div>
+  );
+}
